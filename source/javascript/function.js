a240168017b63e35172f0d081e36d4ebe5d601b6
usmonovsardor7770 <usmonovsardor7770@gmail.com>
2017-09-23 22:19:41 -0400

    Testing Page load speed

--- source/javascript/function.js
+++ source/javascript/function.js
@@ -15,12 +15,6 @@ $(document).ready(function() {
     }
   });
 
-  // Carousel
-  $("#Glide").glide({
-    type: "carousel",
-    autoplay: "5000",
-    startAt: "2"
-  });
 
   // Hamburger toggler
   $(".menu-toggle").on("click", function() {
@@ -34,47 +28,25 @@ $(document).ready(function() {
   var d = new Date();
   var n = d.getFullYear();
   document.getElementById("year").innerHTML = n;
-  
+
   // Scroll on click the nav link
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ?
-          target :
-          $("[name=" + this.hash.slice(1) + "]");
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $("html, body").animate({
-              scrollTop: target.offset().top
-            },
-            1000,
-            function() {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) {
-                // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              }
-            }
-          );
-        }
-      }
-    });
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  // Gallery
+    pageAccelerator();
 });
