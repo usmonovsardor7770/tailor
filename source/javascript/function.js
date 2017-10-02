1ddbd11c5b3a9827037637e170dd37808a4fcf10
usmonovsardor7770 <usmonovsardor7770@gmail.com>
2017-10-02 19:50:12 -0400

    Added loader and Testing

--- source/javascript/function.js
+++ source/javascript/function.js
@@ -58,3 +58,16 @@ $(document).ready(function() {
 
   //
 });
+
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+      },500);
+  }
+}
